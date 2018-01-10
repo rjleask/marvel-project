d0c12d3e7d3fc32c68e5ef388bf6e956303609a8
Jamie <rjamie.leask@gmail.com>
2018-01-10 14:53:10 -0500

    initial push

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import "./App.css";
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/home" component={Home} />
+    </div>
+  </Router>
+);
+
+export default App;
