af56db4c71581dfd645a77438a833270f41e7589
Jamie <rjamie.leask@gmail.com>
2018-03-12 16:13:53 -0400

    deleted trash

---
+++ client/src/keys/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  marvel: {
+    public: "9e7c676a7c13e64bae9b4c03ead26828",
+    private: "028fba96261349e50dc0501868d87f6b85d5bf01"
+  }
+};
--- routes/api/index.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-const homeRoute = require("./home");
-
-router.use("/home", homeRoute);
-module.exports = router;
